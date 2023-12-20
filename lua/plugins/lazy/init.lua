d16d4e64bfd7fee45f404540ae0c158554f40b1b
blueberry-wave <khan.ssjc@gmail.com>
2023-12-20 12:33:38 -0500

    feat: update floating border for lazy window ui

--- lua/plugins/lazy/init.lua
+++ lua/plugins/lazy/init.lua
@@ -5,9 +5,9 @@ print("loading lazy.nvim lua module")
 -- bootstrap lazy plugin manager
 require("plugins.lazy.bootstrap")
 local Lazy = require("lazy")
-
+local Opts = require("plugins.lazy.opts")
 -- lazy config for plugins
 local PluginTable = require("plugins.lazy.plugin_table")
 
 -- Lazy.setup({})
-Lazy.setup(PluginTable)
+Lazy.setup(PluginTable, Opts)
